bb195de7af397b9b4a859af666d39857f7b4bd24
Matthew Milici <milici.m915@gmail.com>
2020-11-14 10:17:41 -0800

    working

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import FriendCard from "../FriendCard/index";
 import friends from "../../friends.json";
+import TableHeader from "../TableHeader/TableHeader"
 
 
 function Table(props) {
-
-
     const [state, setState] = useState({
         sortType: ""
     })
@@ -30,10 +29,14 @@ function Table(props) {
         <table className="table-auto m-auto justify-content-center">
             <thead>
                 <tr className="py-5">
-                    <th onClick={compare} className="px-4 py-2 bg-dark text-white" value="name">Name</th>
-                    <th onClick={compare} className="px-4 py-2 bg-dark text-white" value="position">Position</th>
+                    <TableHeader click={compare} name="Name"></TableHeader>
+                    <TableHeader click={compare} name="Position"></TableHeader>
+                    <TableHeader click={compare} name="Location"></TableHeader>
+                    <TableHeader click={compare} name="Phone"></TableHeader>
+                    {/* <th onClick={compare} className="px-4 py-2 bg-dark text-white" value="name">Name</th> */}
+                    {/* <th onClick={compare} className="px-4 py-2 bg-dark text-white" value="position">Position</th>
                     <th onClick={compare} className="px-4 py-2 bg-dark text-white" value="location">Location</th>
-                    <th onClick={compare} className="px-4 py-2 bg-dark text-white" value="phone">Phone</th>
+                    <th onClick={compare} className="px-4 py-2 bg-dark text-white" value="phone">Phone</th> */}
                 </tr>
             </thead >
             <tbody>
@@ -52,58 +55,4 @@ function Table(props) {
         </table >
     )
 }
-
 export default Table;
-
-
-
-
-
-// import React from "react";
-// import FriendCard from "../FriendCard/index";
-// // import TableHeader from "../TableHeader/TableHeader"
-// import friends from "../../friends.json";
-
-
-// function Table(props) {
-
-//     function checkName(friends) {
-//         return friends.name.includes(props.search);
-//     }
-
-//     function compare(event) {
-//         console.log(event.target.innerText.toLowerCase())
-//         const testValue = event.target.innerText.toLowerCase()
-//         friends.sort((a, b) => (a.testValue > b.testValue) ? 1 : -1)
-
-//     }
-
-
-//     return (
-//         <table className="table-auto m-auto justify-content-center">
-//             <thead>
-//                 <tr className="py-5">
-//                     <th onClick={compare} className="px-4 py-2 bg-dark text-white" value="name">Name</th>
-//                     <th onClick={compare} className="px-4 py-2 bg-dark text-white" value="position">Position</th>
-//                     <th onClick={compare} className="px-4 py-2 bg-dark text-white" value="location">Location</th>
-//                     <th onClick={compare} className="px-4 py-2 bg-dark text-white" value="phone">Phone</th>
-//                 </tr>
-//             </thead >
-//             <tbody>
-//                 {
-//                     friends.filter(checkName).map(friend => (
-//                         <FriendCard
-//                             id={friend.id}
-//                             key={friend.id}
-//                             name={friend.name}
-//                             phone={friend.phone}
-//                             position={friend.position}
-//                             location={friend.location}
-//                         />
-//                     ))}
-//             </tbody>
-//         </table >
-//     )
-// }
-
-// export default Table;
--- src/components/TableHeader/TableHeader.js
+++ src/components/TableHeader/TableHeader.js
@@ -2,11 +2,8 @@ import React from "react";
 
 function TableHeader(props) {
 
-    function test() {
-        console.log(props.children)
-    }
     return (
-        <th onClick={test} className="px-4 py-2 bg-dark text-white">{props.children}</th>
+        <th onClick={props.click} className="px-4 py-2 bg-dark text-white">{props.name}</th>
     )
 }
 export default TableHeader;
